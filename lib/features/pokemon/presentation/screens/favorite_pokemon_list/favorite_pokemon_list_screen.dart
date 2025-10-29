8343bb28e3d6c3a5ad4857836580ea0eab8df146
MarioSQuilantan <marios0013@outlook.com>
2025-10-29 15:56:23 -0600

    feat: favorite pokemon list screen

--- lib/features/pokemon/presentation/screens/favorite_pokemon_list/favorite_pokemon_list_screen.dart
+++ lib/features/pokemon/presentation/screens/favorite_pokemon_list/favorite_pokemon_list_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:pokedex/features/pokemon/pokemon.dart';
 
 import '../../../../../core/core.dart';
 
@@ -11,8 +14,53 @@ class FavoritePokemonListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFFFF5959),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text('Pokédex', style: TextStyle(color: Colors.black)),
+      ),
       body: SafeArea(
-        child: Padding(padding: const EdgeInsets.all(8.0), child: Container()),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BlocBuilder<GetFavoritePokemonListCubit, GetFavoritePokemonListState>(
+            builder: (context, state) {
+              if (state.status == NetworkStatusEnum.isLoading) {
+                return const Center(child: CircularProgressIndicator());
+              }
+
+              if (state.status == NetworkStatusEnum.isError) {
+                return Center(child: Text(state.errorMessage));
+              }
+
+              if (state.status == NetworkStatusEnum.isSuccess) {
+                final displayedList = state.favoritePokemonList;
+
+                return GridView.builder(
+                  itemCount: displayedList.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 12,
+                    childAspectRatio: 0.75,
+                  ),
+                  itemBuilder: (context, index) {
+                    final pokemon = displayedList[index];
+
+                    return ImageCardWidget(
+                      key: ValueKey(pokemon.id),
+                      pokemon: pokemon,
+                      onTap: () {
+                        context.pushNamed(PokemonDetailScreen.name, pathParameters: {'id': '${pokemon.id}'});
+                      },
+                    );
+                  },
+                );
+              }
+
+              return const SizedBox.shrink();
+            },
+          ),
+        ),
       ),
     );
   }
--- lib/features/pokemon/presentation/views/views.dart
+++ lib/features/pokemon/presentation/views/views.dart
@@ -1,3 +1,1 @@
-export 'favorite_pokemon_list/favorite_pokemon_list.dart';
 export 'pokemon_detail/pokemon_detail.dart';
-export 'pokemon_list/pokemon_list.dart';
